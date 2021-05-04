87adf231b0fb29063617e4055bd0405f5489dc64
AndresAco92 <andres.antonio.aco92@gmail.com>
2021-05-04 20:46:51 +0200

    Esercizio finito...

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,20 @@ const app = new Vue({
   el: '#root',
   data:{
     title: 'VueProgrammer!',
-    text_color:''
-  },
+    text_color: '',
+    text_simple: '',
+    picture : ''
+  },  
   methods:{
-    changeColor(colore){
-      this.text_color = colore;
-    }
+    changeColor(val){
+      this.text_color = val;
+    },
+    color_new(){
+      this.text_color = this.text_simple;
+    },
+    // text_other_color(){
+    //   this.text_color = this.color_simple;
+    // }
   }
 
 });
